20427a2bb98a2116e922c47809438b2d1b7f7cdd
Mufil Rahman A <mufil.r@simplicontract.com>
2024-10-02 18:56:14 +0530

    finding largest number

---
+++ mufil/examples/largest_number.py
@@ -0,0 +1,12 @@
+a = 5
+b= 10
+
+# Find the largest number
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater than {a}")
+
+
+#ternary operator
+print(f"largest number is {a if a > b else b}")
