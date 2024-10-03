fe9b7d03526e8f1b1ad797e4aa27ec5d5920a098
muhemmadvp <muhemad1303@gmail.com>
2024-10-03 21:29:58 +0530

    add program to find largest of two numbers

---
+++ muhammed/example/largest_number.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+
+if num1 > num2:
+    print(f"the largest number is{num1}")
+else:
+    print(f"the largest number is {num2}")
