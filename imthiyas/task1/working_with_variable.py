cd0b844b4cd1e99e0b7eb4bf971686f4dc5e736a
imthiyas <imthiyas098@gmail.com>
2024-10-06 19:49:32 +0530

    bug fixed

--- imthiyas/task1/working_with_variable.py
+++ imthiyas/task1/working_with_variable.py
@@ -4,3 +4,4 @@ is_student = True
 print(f"i am {name} i am {age} years old, is student {is_student}")
 is_student = False
 print(f"i am {name} i am {age} years old, is student {is_student}")
+#requested changes done
