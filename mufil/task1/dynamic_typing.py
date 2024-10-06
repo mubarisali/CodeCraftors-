6e9f7bab0749274c93a4d4549909dd4c69a561ac
Mufil Rahman A <mufil.r@simplicontract.com>
2024-10-06 12:44:50 +0530

    task1 completed

---
+++ mufil/task1/dynamic_typing.py
@@ -0,0 +1,7 @@
+# Assign an integer value first
+data = 100
+print(f"Initial value of data: {data}, Type: {type(data)}")
+
+# Change the value to a string
+data = "Python"
+print(f"Updated value of data: {data}, Type: {type(data)}")
