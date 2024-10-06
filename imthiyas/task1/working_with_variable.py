b0be1c44320b42d0b6f8a0df44dddd53e4a781d4
imthiyas <imthiyas098@gmail.com>
2024-10-06 19:40:57 +0530

    done with working with variable problem

---
+++ imthiyas/task1/working_with_variable.py
@@ -0,0 +1,6 @@
+name = "imthiyas"
+age = 26
+is_student = True
+print(f"i am {name} i am {age} years old, is student {is_student}")
+is_student = False
+print(f"i am {name} i am {age} years old, is student {is_student}")
