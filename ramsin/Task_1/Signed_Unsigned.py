e466a1137235bca8cc711f59458f47fc0e6e5add
Ramsin Rassal P G <ramsinrazzal@gmail.com>
2024-10-09 14:36:30 +0530

    completed my first task

---
+++ ramsin/Task_1/Signed_Unsigned.py
@@ -0,0 +1,8 @@
+num = int(input("enter a number"))
+if num >= 0 :
+ print(f"The entered number {num} is valid (unsigned integer).")
+
+else :
+  print("Error: Negative numbers are not allowed! Please enter a positive integer.")
+
+    
---
+++ ramsin/Task_1/dnamic_typing.py
@@ -0,0 +1,4 @@
+data = 10
+print(type(data))
+data = "Ramsin Rassal"
+print(type(data))
