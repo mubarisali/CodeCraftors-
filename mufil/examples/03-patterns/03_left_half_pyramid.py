a1a2d73777b6492ee8f62015f149cb98dfdc9ba2
Mufil Rahman A <mufil.r@simplicontract.com>
2024-10-10 13:41:40 +0530

    more patterns

---
+++ mufil/examples/03-patterns/03_left_half_pyramid.py
@@ -0,0 +1,17 @@
+# write a program to print the following pattern
+
+"""
+        *    
+      * *    
+    * * *    
+  * * * *    
+* * * * *    
+"""
+for i in range ( 1, 6 ):
+    str = ""
+    for j in range ( 1, 6 - i ):
+        str += "  "
+    for j in range ( 1, i + 1 ):
+        str += "* "
+    print(str)
+
