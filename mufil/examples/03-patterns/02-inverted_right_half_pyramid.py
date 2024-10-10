512385de6a73ee66920dd9400f038deda44edaad
Mufil Rahman A <mufil.r@simplicontract.com>
2024-10-10 13:47:41 +0530

    different way of answering

--- mufil/examples/03-patterns/02-inverted_right_half_pyramid.py
+++ mufil/examples/03-patterns/02-inverted_right_half_pyramid.py
@@ -7,10 +7,17 @@
 * *      
 *       
 """
-
+# solution 1
 for i in range ( 1, 6 ):
     str = ""            
     for j in range ( 1, 7 - i ):
         str += "* "
     print(str)
 
+# solution 2
+
+for i in range (6):
+    str = ""            
+    for j in range (5-i):
+        str += "* "
+    print(str)
