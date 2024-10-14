26137a93ba68968e998bc48e45e7ee39eaa8a630
Ramsin Rassal P G <ramsinrazzal@gmail.com>
2024-10-14 12:38:28 +0530

    completed the second task

---
+++ ramsin/Task_2/prime_no_check.py
@@ -0,0 +1,18 @@
+
+n = int(input("Enter a number to check if it's prime: "))  
+is_prime = True  
+
+
+if n <= 1:
+    is_prime = False 
+else:
+    for i in range(2, int(n ** 0.5) + 1):  
+        if n % i == 0:  
+            is_prime = False
+            break  
+
+
+if is_prime:
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is not a prime number.")
