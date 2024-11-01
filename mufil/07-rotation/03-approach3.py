443fe7e83540a321af4147f6ccf995b10bfb957d
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-01 20:10:40 +0530

    approach 3 and 4

---
+++ mufil/07-rotation/03-approach3.py
@@ -0,0 +1,17 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Try to Solve in time Complexity 0(n) without any extra Space.
+You Dont need to return an Array. Modify array in place. 
+"""
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+
+def rotate(nums, k):
+    pass
+
+
+
+
+rotate(nums, k)
+print(nums)
