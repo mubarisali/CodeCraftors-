7523eac9266d4b3c8c65727ed7b6a549d4e0e87e
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-04 21:29:59 +0530

    ans updated

--- mufil/07-rotation/03-approach3.py
+++ mufil/07-rotation/03-approach3.py
@@ -10,18 +10,18 @@ k = 2
 def rotate(nums, k):
     k = k
     cur_index = 0
-    val = nums[0]
+    temp = nums[0]
     count = 0
     cycle_position = 0
     while count < len(nums):
         next_index = (cur_index + k) % len(nums)
-        val, nums[next_index] = nums[next_index], val
+        temp, nums[next_index] = nums[next_index], temp
         cur_index = next_index
         count += 1
         if (cur_index == cycle_position):
             cur_index = (cur_index + 1) % len(nums)
             cycle_position = cur_index
-            val = nums[cur_index]
+            temp = nums[cur_index]
 
 rotate(nums, k)
 print(nums)
